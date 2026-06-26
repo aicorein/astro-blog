d3f172e3c3b387650ad63b11cbb07c87db947812
aicorein <aicorein@meloland.org>
2026-06-26 18:02:02 +0800

    chore: update avatar images and cleanup assets url

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -2,7 +2,7 @@ import type { ProfileConfig } from "../types/config";
 
 // 个人资料配置
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "/static/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "律回",
 	bio: "我们弹下的每段旋律，终将在某个时刻回归",
 	typewriter: {
